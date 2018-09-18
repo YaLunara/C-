5ce3e1945bb230a1cc81b009288c27a6376368d7
YaLunara <1134996823@qq.com>
2018-09-18 17:52:13 +0800

    Program.cs

--- homework2/homework2/Program.cs
+++ homework2/homework2/Program.cs
@@ -7,10 +7,33 @@ namespace homework2
 {
     class Program
     {
+        static bool isNum(string s)
+        {
+            if (s == null || s.Length == 0)
+                return false;
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            byte[] bytes = ascii.GetBytes(s);
+            foreach(byte c in bytes)
+            {
+                if( c < 48 || c > 57)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("请输入一个数：");
             string s = Console.ReadLine();
+            if (!isNum(s))
+            {
+                do
+                {
+                    Console.WriteLine("输入不规范");
+                    s = Console.ReadLine();
+                } while (!isNum(s));
+            }
             int n = int.Parse(s);
             Console.WriteLine("该数的素数因子为：");
             int i = 2;
