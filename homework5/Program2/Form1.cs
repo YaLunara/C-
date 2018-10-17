c2794cc565ff5a02562d3260fc827324ff5e52f1
YaLunara <1134996823@qq.com>
2018-10-17 20:02:08 +0800

    homework5

--- homework5/Program2/Form1.cs
+++ homework5/Program2/Form1.cs
@@ -11,31 +11,42 @@ namespace Program2
 {
     public partial class Form1 : Form
     {
-       
-        public Form1()
+
+       public Form1()
         {
             InitializeComponent();
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            if (graphics == null) graphics = this.CreateGraphics();
+            if (graphics != null)
+            {
+                graphics.Clear(this.BackColor);
+                graphics.Dispose();
+            }
+            graphics = this.CreateGraphics();
             drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
         }
+            
 
         private Graphics graphics;
-        double per1 = 0.6;
-        double per2 = 0.7;
 
         void drawCayleyTree (int n, double x0, double y0, double leng, double th)
         {
             if (n == 0) return;
 
+            Random random = new Random();
+
+            double per1 = (double)(random.Next(59, 80)) / 100;
+            double per2 = (double)(random.Next(59, 80)) / 100;
+            Console.WriteLine(per1);
+            Console.WriteLine(per2);
+            double th1 = random.Next(25, 60) * Math.PI / 180;
+            double th2 = random.Next(25, 60) * Math.PI / 180;
+
             double x1 = x0 + leng * Math.Cos(th);
             double y1 = y0 + leng * Math.Sin(th);
             drawLine(x0, y0, x1, y1);
-            Random random = new Random();
-            double th1 = random.Next(25, 60) * Math.PI / 180;
-            double th2 = random.Next(25, 60) * Math.PI / 180;
+            
             drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
             drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
             
