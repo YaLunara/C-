54d8bd2a2fa9ccaf36e44303887448fc4cabfaad
YaLunara <1134996823@qq.com>
2018-10-31 23:42:27 +0800

    homework7

---
+++ Homework7/Homework7/FrmSearchOd.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Homework7
+{
+    partial class FrmSearchOd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SearchBy = new System.Windows.Forms.ComboBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SearchBy
+            // 
+            this.SearchBy.FormattingEnabled = true;
+            this.SearchBy.Items.AddRange(new object[] {
+            "全部订单",
+            "OrderId",
+            "CustomerName",
+            "GoodsName"});
+            this.SearchBy.Location = new System.Drawing.Point(115, 41);
+            this.SearchBy.Name = "SearchBy";
+            this.SearchBy.Size = new System.Drawing.Size(177, 23);
+            this.SearchBy.TabIndex = 0;
+            this.SearchBy.Text = "全部订单";
+            this.SearchBy.SelectedIndexChanged += new System.EventHandler(this.SearchBy_SelectedIndexChanged);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(32, 44);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(52, 15);
+            this.label.TabIndex = 1;
+            this.label.Text = "关键字";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "输入信息";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(115, 112);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(177, 25);
+            this.txtKeyWord.TabIndex = 3;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(35, 192);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(257, 33);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // FrmSearchOd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 291);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtKeyWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.SearchBy);
+            this.Name = "FrmSearchOd";
+            this.Text = "FrmSearchOd";
+            this.Load += new System.EventHandler(this.FrmSearchOd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox SearchBy;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
---
+++ Homework7/Homework7/MainForm1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Homework7
+{
+    partial class MainForm1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.orderList = new System.Windows.Forms.ListView();
+            this.orderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.customer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Amount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.orderDetailId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.goodsName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.quantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(37, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 56);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "添加订单";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(37, 166);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(120, 56);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "删除订单";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(37, 287);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(120, 56);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "修改订单";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(37, 400);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(120, 56);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "查询订单";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // orderList
+            // 
+            this.orderList.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.orderList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.orderId,
+            this.customer,
+            this.Amount,
+            this.orderDetailId,
+            this.Id,
+            this.goodsName,
+            this.Value,
+            this.quantity});
+            this.orderList.HotTracking = true;
+            this.orderList.HoverSelection = true;
+            this.orderList.Location = new System.Drawing.Point(248, 50);
+            this.orderList.Name = "orderList";
+            this.orderList.Size = new System.Drawing.Size(807, 445);
+            this.orderList.TabIndex = 4;
+            this.orderList.UseCompatibleStateImageBehavior = false;
+            this.orderList.View = System.Windows.Forms.View.Details;
+            this.orderList.SelectedIndexChanged += new System.EventHandler(this.orderList_SelectedIndexChanged);
+            // 
+            // orderId
+            // 
+            this.orderId.Text = "orderId";
+            this.orderId.Width = 70;
+            // 
+            // customer
+            // 
+            this.customer.Text = "customer";
+            this.customer.Width = 80;
+            // 
+            // Amount
+            // 
+            this.Amount.Text = "Amount";
+            this.Amount.Width = 70;
+            // 
+            // orderDetailId
+            // 
+            this.orderDetailId.Text = "orderDetailId";
+            this.orderDetailId.Width = 120;
+            // 
+            // Id
+            // 
+            this.Id.Text = "Id";
+            this.Id.Width = 40;
+            // 
+            // goodsName
+            // 
+            this.goodsName.Text = "goodsName";
+            this.goodsName.Width = 100;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            // 
+            // quantity
+            // 
+            this.quantity.Text = "quantity";
+            this.quantity.Width = 80;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 560);
+            this.Controls.Add(this.orderList);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "HandleServices";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnSearch;
+        public System.Windows.Forms.ListView orderList;
+        private System.Windows.Forms.ColumnHeader orderId;
+        private System.Windows.Forms.ColumnHeader customer;
+        private System.Windows.Forms.ColumnHeader Amount;
+        private System.Windows.Forms.ColumnHeader orderDetailId;
+        private System.Windows.Forms.ColumnHeader Id;
+        private System.Windows.Forms.ColumnHeader goodsName;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ColumnHeader quantity;
+    }
+}
+
